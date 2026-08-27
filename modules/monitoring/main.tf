e4c65da91407613388d98cfc1fae21d5d04be3a9
Gaurav kumar <gauravk.evolve@gmail.com>
2026-08-28 00:36:02 +0530

    Add CloudWatch alarms for application infrastructure

--- modules/monitoring/main.tf
+++ modules/monitoring/main.tf
@@ -1,1 +1,46 @@
-resource "aws_cloudwatch_log_group" "app" { name = "/three-tier/application" retention_in_days = 14 }
+resource "aws_cloudwatch_log_group" "app" {
+  name              = "/three-tier/application"
+  retention_in_days = 14
+}
+
+resource "aws_cloudwatch_metric_alarm" "asg_cpu" {
+  alarm_name          = "three-tier-asg-high-cpu"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 2
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = 300
+  statistic           = "Average"
+  threshold           = 70
+  alarm_description   = "Application ASG average CPU is above 70 percent"
+  dimensions = { AutoScalingGroupName = var.asg_name }
+}
+
+resource "aws_cloudwatch_metric_alarm" "alb_5xx" {
+  alarm_name          = "three-tier-alb-5xx"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 2
+  metric_name         = "HTTPCode_ELB_5XX_Count"
+  namespace           = "AWS/ApplicationELB"
+  period              = 300
+  statistic           = "Sum"
+  threshold           = 5
+  alarm_description   = "ALB returned more than five 5XX responses"
+  dimensions = { LoadBalancer = var.alb_arn_suffix }
+}
+
+resource "aws_cloudwatch_metric_alarm" "target_5xx" {
+  alarm_name          = "three-tier-target-5xx"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 2
+  metric_name         = "HTTPCode_Target_5XX_Count"
+  namespace           = "AWS/ApplicationELB"
+  period              = 300
+  statistic           = "Sum"
+  threshold           = 5
+  alarm_description   = "Application target returned more than five 5XX responses"
+  dimensions = {
+    LoadBalancer = var.alb_arn_suffix
+    TargetGroup  = var.target_group_arn_suffix
+  }
+}
